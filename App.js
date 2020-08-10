0d8070fdd6d431499919e1550f0f90d4494d16dd
Jose L Junior Garza Zuñiga <correojunior@gmail.com>
2020-08-10 03:54:56 -0500

    Tab colors added

--- App.js
+++ App.js
@@ -5,75 +5,80 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Feather } from '@expo/vector-icons';
 
 import { MainScreen, SettingsScreen } from './screens';
-import { theme, ThemeContextProvider} from './theme/themeProvider';
+import { withTheme, ThemeContextProvider } from './theme/themeProvider';
 
 const Tab = createBottomTabNavigator();
-const customTabBarStyle = {
-  activeTintColor: theme.backgroundColor,
-  inactiveTintColor: 'gray',
-  allowFontScaling: true,
-  labelStyle: { fontSize: 16, paddingTop: 5 },
-  tabStyle: { paddingTop: 5 },
-  style: { height: 60, borderTopColor: theme.color },
-}
-const MyTabs = () => {
-  return (
-    <Tab.Navigator
-    tabBarOptions={customTabBarStyle}
-    
-  /*    screenOptions={({ route }) => ({
-        tabBarIcon: ({ color, size }) => {
-          const icons = {
-            Home: 'home',
-            Settings: 'settings',
-          };
+const customTabBarStyle = withTheme(
+  ({ theme }) => {
+    return {
+      activeTintColor: theme.backgroundColor,
+      inactiveTintColor: 'gray',
+      allowFontScaling: true,
+      tabStyle: { paddingTop: 5 },
+      style: { height: 60, borderTopColor: theme.color },
+    }
+  })
+const MyTabs = withTheme(
+  ({ theme }) => {
+    return (
+      <Tab.Navigator
+        tabBarOptions={customTabBarStyle}
 
-          return (
-            <Feather
-              name={icons[route.name]}
-              color={color}
-              size={size}
-            />
-          );
-        },
-      })}*/
-    > 
-      <Tab.Screen
-        name="Home"
-        component={MainScreen}
-        options={{
-          tabBarIcon: ({ focused, color, size }) => (
-            <Feather 
-            name="home" 
-            focused={focused}
-            color={focused ? theme.backgroundColor : 'gray'} 
-            size={size} />
-          ),
-        }} 
-      />
-      <Tab.Screen
-        name="Settings"
-        component={SettingsScreen}
-        options={{
-          tabBarIcon: ({ focused, color, size }) => (
-            <Feather 
-            name="settings" 
-            focused={focused}
-            color={focused ? theme.backgroundColor : 'gray'} 
-            size={size} />
-          ),
-        }} 
-      />
-    </Tab.Navigator>
-  );
-}
+      /*    screenOptions={({ route }) => ({
+            tabBarIcon: ({ color, size }) => {
+              const icons = {
+                Home: 'home',
+                Settings: 'settings',
+              };
+    
+              return (
+                <Feather
+                  name={icons[route.name]}
+                  color={color}
+                  size={size}
+                />
+              );
+            },
+          })}*/
+      >
+        <Tab.Screen
+          name="Home"
+          component={MainScreen}
+          options={{
+            tabBarLabel: "Home",
+            tabBarIcon: ({ focused, color, size }) => (
+              <Feather
+                name="home"
+                focused={focused}
+                color={focused ? theme.backgroundColor : 'gray'}
+                size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Settings"
+          component={SettingsScreen}
+          options={{
+            tabBarIcon: ({ focused, color, size }) => (
+              <Feather
+                name="settings"
+                focused={focused}
+                color={focused ? theme.backgroundColor : 'gray'}
+                size={size} />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    );
+  }
+)
 
 export default () => {
   return (
     <ThemeContextProvider>
-    <NavigationContainer>
-      <MyTabs />
-    </NavigationContainer>
+      <NavigationContainer>
+        <MyTabs />
+      </NavigationContainer>
     </ThemeContextProvider>
   );
 }
